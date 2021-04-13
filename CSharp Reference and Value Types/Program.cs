632d47d49c61d7710a2c6d534538c55cdbc68470
Jake <jakenguyen0118@gmail.com>
2021-04-13 12:57:31 -0400

    more reference/value type examples

--- CSharp Reference and Value Types/Program.cs
+++ CSharp Reference and Value Types/Program.cs
@@ -2,10 +2,15 @@
 
 namespace CSharp_Reference_and_Value_Types
 {
+    public class Person
+    {
+        public int Age;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            /*
             // value type
             // does not affect each other in memory
             var a = 10;
@@ -23,7 +28,26 @@ namespace CSharp_Reference_and_Value_Types
             // when copied they are referencing from the same memory, and not copying the value like value types
             // any changes made for array1 or array2 will change both
             Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+            */
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+            // value type so the var number will remain as 1
 
+            var person = new Person() {Age = 20};
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+            // reference type so person's age will increment to 30
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
         }
     }
 }
