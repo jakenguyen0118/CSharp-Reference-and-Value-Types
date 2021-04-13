f7327b870ff0d72681944c3968db87710eeba4f5
Jake <jakenguyen0118@gmail.com>
2021-04-13 12:48:08 -0400

    Add project files.

---
+++ CSharp Reference and Value Types/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharp_Reference_and_Value_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // value type
+            // does not affect each other in memory
+            var a = 10;
+            var b = a;
+            b++;
+
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+
+            // reference type
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+
+            // first element of array1 will change to 0
+            // when copied they are referencing from the same memory, and not copying the value like value types
+            // any changes made for array1 or array2 will change both
+            Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+
+        }
+    }
+}
